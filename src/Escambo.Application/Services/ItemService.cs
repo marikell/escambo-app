4bac9c9ea02134f62f11527cb3d31bdfafed46d1
Marianne Albuquerque da Silva <marikell_silva@hotmail.com>
2019-03-16 18:28:13 -0300

    Increasing project structure

---
+++ src/Escambo.Application/Services/ItemService.cs
@@ -0,0 +1,13 @@
+using System;
+using Escambo.Domain.Models;
+
+namespace Escambo.Application.Services
+{
+    public class ItemService
+    {
+        public void ExchangeItem(Item item1, Item item2)
+        {
+            throw new Exception("Not implemented!");
+        }
+    }
+}
